6e721933d9d7f1ab64abc91bc1498c2541f6e7cd
Marcelo Cardoso <marcelocardoso74@gmail.com>
2024-09-24 12:53:43 -0300

    Define Layout And Consumming Microservice

---
+++ S10-MicroServices_DorNet6_DefineLayoutAndConsummingMicroservice/geekShopping/GeekShopping.ProductAPI/Migrations/20240920170301_SeedDataProductDataTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedDataProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "Id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 4L, "Action Figure", "NAVE DO HAN SOLO MILENNIUM FALCON", "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true", "Star Wars Mission Fleet Han Solo Nave Milennium Falcon", 359.99m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "Id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 5L, "T-shirt", "Camista de comemoração de 30 anos do seriado FRIENDS", "https://github.com/marcelocardoso74/curso-microservices-dotnet6/blob/main/Imagens/camisetafriends.png?raw=true", "Camiseta Comemorativa FRIENDS", 69.9m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "Id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 6L, "Action Figure", "capacete comemorativo Darth Vader", "https://github.com/marcelocardoso74/curso-microservices-dotnet6/blob/main/Imagens/capacetedarthvader.jpg?raw=true", "Capacete Darth Vader Star Wars Black Series", 999.99m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 6L);
+        }
+    }
+}
---
+++ S9-MicroServices_DorNet6_ConsummingProductMicroserviceWebAPI/geekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+
+        Task<ProductModel> FindAllProductByID(long id);
+
+        Task<ProductModel> CreateProduct(ProductModel model);
+
+        Task<ProductModel> UpdateProduct(ProductModel model);
+
+        Task<bool> DeleteProductByID(long id);
+    }
+}
